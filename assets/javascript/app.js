2f1e2d33930fd2fc25fbcff75607015cac03e0f3
Tals <natalia.mahabir@gmail.com>
2018-07-12 00:20:27 -0400

    images, css, html, js updated

---
+++ assets/javascript/app.js
@@ -0,0 +1,120 @@
+// Make sure document has loaded
+$(document).ready(function(){
+
+    // Variables
+
+    var questions = [
+        "Who's pick up line is 'How you doing?'?",
+        "What did Ross say when Joey asked him if that's not how a taylor measure's pants",
+        "What did Joey grab when a car backfired when the three guys were out with Phoebe's Policeman?",
+        "What's the name of the trophy Monica and Ross played football for every thankgiving?",
+        "What does Phoebe tell Rachel is wrong with the plane?",
+        "What's the name of the gameshow Joey had to pratice for an Audition?",
+        "Where was Chandler trapped during the blackout?",
+        "Who's saying is 'Oh! My! God!'?",
+    ];
+    
+    var answers = [
+        ["Joey", "Ross", "Phoebe", "Janice"],
+        ["In the church!", "Yes, In Prison!","If your tayor was your mom and you're a baby", "Yup at the doctors"],
+        ["Ross", "Chandler", "Pizza", "Sandwich"],
+        ["Monica and Ross Cup", "Gella Cup", "Thanksgiving Cup", "World Cup"],
+        ["Wing", "Pilot", "Phalange", "Wheels"],
+        ["Dynamite", "Bamboozled", "Damboozled", "Friends Feud"],
+        ["His Office", "Centrel Perk", "ATM Vestibule", "The apartment"],
+        ["Rachel", "Janice", "Chandler","Joey"],
+    ];
+
+    var correctResponses = [
+        "Joey", "Yes, In Prison!", "Sandwich", "Gella Cup", "Phalange", "Bamboozled", "ATM Vestibule", "Janice"
+    ];
+
+    var gameover = {
+        wins: 0,
+        losses: 0,
+        unanswered: 0,
+    };
+
+    var timeLeft = 120;
+    var userInput = [];
+    // var i = 0;
+
+    function stopTimer(){
+        timeLeft = 120;
+        clearInterval(timeLeft);
+    };
+
+
+    $("#startButton").click(function(){
+        timeLeft = setInterval(timeRemaining, 1000);
+        question();
+        $("#startButton").remove();
+
+
+    });
+
+    
+    // var counter = 0;
+ 
+    
+
+    function timeRemaining(){
+        $("#timeRemaining").html("Time Remaining: " + timeLeft);
+        timeLeft--;
+
+        if (timeLeft === 0){
+            $("#wins").html("Wins: " + gameover.wins);
+            $("#losses").html("Losses: " + gameover.losses);
+            $("#wins").html("Unanswered: " + gameover.unanswered);
+            stopTimer();
+
+        }
+    };
+
+    
+
+    function question(){
+        
+        for (i=0; i < questions.length; i++) {
+            console.log(questions[i]);
+            $("#question").html(questions[i]);
+            $("#answers").html(
+                "<form>" + 
+                    "<input type='radio'>" + answers[i][0] + "<br>" +
+                    "<input type='radio'>" + answers[i][1] + "<br>" +
+                    "<input type='radio'>" + answers[i][2] + "<br>" +
+                    "<input type='radio'>" + answers[i][3] + "<br>" +
+                    "<button> Next </button>" +
+                "</form>");
+
+            // Something to tag the user click and check it with the correctresponses array
+
+            // if (userInput[i])
+        }   
+        
+    };
+    
+    // function checker(){
+    //     if (userInput)
+    // }
+
+
+
+
+    
+
+    
+    
+
+
+
+
+
+
+})
+
+
+
+
+
+
