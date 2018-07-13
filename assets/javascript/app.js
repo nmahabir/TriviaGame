cecbedbd8a09b0d29e015af0b2b412dedc245697
Tals <natalia.mahabir@gmail.com>
2018-07-12 23:46:54 -0400

    updated functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
   var questions = [
     "Who's pick up line is 'How you doing?'?",
-    "What did Ross say when Joey asked him if that's not how a taylor measure's pants",
+    "What did Ross say when Joey asked him if that's not how a taylor measure's pants?",
     "What did Joey grab when a car backfired when the three guys were out with Phoebe's Policeman?",
     "What's the name of the trophy Monica and Ross played football for every thankgiving?",
     "What does Phoebe tell Rachel is wrong with the plane?",
@@ -46,39 +46,29 @@ $(document).ready(function() {
     unanswered: 0
   };
 
-  var timeLeft = 120;
+  //   var timeLeft = 120;
   var userInput = [];
 
-  function stopTimer() {
-    clearInterval(timeLeft);
-    timeLeft = 120;
-    $("#timeRemaining").html("Time Remaining: " + timeLeft);
-  }
+  //   function stopTimer() {
+  //     clearInterval(timeLeft);
+  //     timeLeft = 120;
+  //     $("#timeRemaining").html("Time Remaining: " + timeLeft);
+  //   }
 
   $("#startButton").click(function() {
-    timeLeft = setInterval(timeRemaining, 1000);
-    question();
+    // timeLeft = setInterval(timeRemaining, 1000);
+    // question();
     $("#startButton").remove();
-  });
 
-  function timeRemaining() {
-    $("#timeRemaining").html("Time Remaining: " + timeLeft);
-    timeLeft--;
-
-    if (timeLeft === 0) {
-      $("#wins").html("Wins: " + gameover.wins);
-      $("#losses").html("Losses: " + gameover.losses);
-      $("#wins").html("Unanswered: " + gameover.unanswered);
-      stopTimer();
-    }
-  }
-
-  function question() {
     for (i = 0; i < questions.length; i++) {
       console.log(questions[i]);
-      $("#question").html(questions[i]);
-      $("#answers").html(
-        "<form>" +
+      var trivia = $("#answers");
+      trivia.html(
+        "<h3 id='questions' align='center'>" +
+          questions[i] +
+          "</h3>" +
+          "<br>" +
+          "<form>" +
           "<input type='radio'>" +
           answers[i][0] +
           "<br>" +
@@ -94,12 +84,72 @@ $(document).ready(function() {
           "<button id='next'> Next </button>" +
           "</form>"
       );
-
-      // Something to tag the user click and check it with the correctresponses array
-
-      // if (userInput[i])
+      $("#answers").append(trivia);
     }
-  }
+  });
+
+  //   function timeRemaining() {
+  //     $("#timeRemaining").html("Time Remaining: " + timeLeft);
+  //     timeLeft--;
+
+  //     if (timeLeft === 0) {
+  //       $("#wins").html("Wins: " + gameover.wins);
+  //       $("#losses").html("Losses: " + gameover.losses);
+  //       $("#wins").html("Unanswered: " + gameover.unanswered);
+  //       stopTimer();
+  //     }
+  //   }
+
+  //   function question() {
+  //     for (i = 0; i < questions.length; i++) {
+  //       console.log(questions[i]);
+  //       $("#answers").html(
+  //         "<h3 id='questions' align='center'>" +
+  //           questions[i] +
+  //           "</h3>" +
+  //           "<br>" +
+  //           "<form>" +
+  //           "<input type='radio'>" +
+  //           answers[i][0] +
+  //           "<br>" +
+  //           "<input type='radio'>" +
+  //           answers[i][1] +
+  //           "<br>" +
+  //           "<input type='radio'>" +
+  //           answers[i][2] +
+  //           "<br>" +
+  //           "<input type='radio'>" +
+  //           answers[i][3] +
+  //           "<br>" +
+  //           "<button id='next'> Next </button>" +
+  //           "</form>"
+  //       );
+  //       console.log(answers[i][0]);
+  //       console.log(answers[i][1]);
+  //       console.log(answers[i][2]);
+  //       console.log(answers[i][3]);
+  //   $("#answers").html(
+  //     "<form>" +
+  //       "<input type='radio'>" +
+  //       answers[i][0] +
+  //       "<br>" +
+  //       "<input type='radio'>" +
+  //       answers[i][1] +
+  //       "<br>" +
+  //       "<input type='radio'>" +
+  //       answers[i][2] +
+  //       "<br>" +
+  //       "<input type='radio'>" +
+  //       answers[i][3] +
+  //       "<br>" +
+  //       "<button id='next'> Next </button>" +
+  //       "</form>"
+  //     }
+  //   }
+
+  // Something to tag the user click and check it with the correctresponses array
+
+  // if (userInput[i])
 
   $("#answers").click(function() {
     userInput = userInput.push(this.value);
